75dd71dd6c6fcc940331bc887d8795867654ab89
3520233 <alexis.rossi@etu.upmc.fr>
2018-12-21 10:36:00 +0100

    Some other changes

--- player.cc
+++ player.cc
@@ -1,6 +1,6 @@
 #include "player.hh"
 
-Player::Player(int x, int y, int hp, std::string name) : Character(x, y, 5, hp), m_name(name) {}
+Player::Player(int x, int y, int hp, std::string name) : Character(x, y, Player::m_player_size, hp), m_name(name) {}
 
 
 
--- player.hh
+++ player.hh
@@ -6,15 +6,15 @@
 class Player : public Character {
 
 	private:
-		std::string m_name;								//Name of the player
-		unsigned int m_score;						//Score of the player
+		std::string m_name;				//Name of the player
+		unsigned int m_score;			//Score of the player
 
 
 	public:
 		static constexpr std::size_t m_player_size = 20;		//Size of a Player object (value to modify)
 
 
-		Player(int x, int y, std::size_t size, int hp, std::string name);
+		Player(int x, int y, int hp, std::string name);
 		virtual ~Player();
 		virtual void move();  
 		virtual Shot* shoot();	
