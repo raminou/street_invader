2817023bcc094395c3c0d1f3af4804053f4d451e
RDROUIN <remi.drouin@etu.upmc.fr>
2019-01-24 12:36:48 +0100

    add cmake + run.bash

---
+++ run.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+SFML_DL_LINK="https://www.sfml-dev.org/files/SFML-2.5.1-linux-gcc-64-bit.tar.gz"
+SFML_FILE_DL="SFML-2.5.1.tar.gz"
+
+# Download SFML
+if [ ! -d "SFML-2.5.1" ] ; then
+	echo "# Downloading SFML from $SFML_DL_LINK"
+	wget "$SFML_DL_LINK" -O "$SFML_FILE_DL"
+	tar zxvf "$SFML_FILE_DL"
+	rm "$SFML_FILE_DL"
+fi
+
+if [ ! -d "build" ] || [ ! -f "build/game" ] ; then
+	if [ ! -f "Makefile" ] ; then
+		echo -e "\n\n\n# Creating Makefile"
+		cmake -G"Unix Makefiles"
+	fi
+	echo -e "\n\n\n# Compiling"
+	make
+fi
+echo -e "\n\n\n# Running"
+./build/game
